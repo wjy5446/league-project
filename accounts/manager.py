9668304178d066b7890c24576190a23aa984f062
wjy5446 <wjy5446@gmail.com>
2020-07-29 00:50:10 +0900

    feat) add custom accounts admin

--- accounts/manager.py
+++ accounts/manager.py
@@ -1,20 +1,25 @@
 import datetime
 
 from django.db import models
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 
-class UserManager(UserManager):
-    def create_user(self, email: str, password, **extra_fields) -> models.Model:
-        
-        assert email, ValueError(_("The Email must be set"))
-
+class UserManager(BaseUserManager):
+    def _create_user(self, email: str, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save()
-
+        user.save(using=self._db)
+        
         return user
+
+    def create_user(self, email: str, password, **extra_fields) -> models.Model:
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email: str, password, **extra_fields) -> models.Model:
 
@@ -22,10 +27,7 @@ class UserManager(UserManager):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
-        extra_fields.setdefault("birth_day", datetime.date(1900, 1, 1))
-        extra_fields.setdefault("nickname", "신")
-
         assert extra_fields.get("is_staff") is True, ValueError(_("Superuser must have is_staff=True."))
         assert extra_fields.get("is_superuser") is True, ValueError(_("Superuser must have is_superuser=True."))
         
-        return self.create_user(email=email, password=password, **extra_fields)
+        return self._create_user(email=email, password=password, **extra_fields)
--- accounts/models.py
+++ accounts/models.py
@@ -2,18 +2,17 @@ import datetime
 
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
 
 from .manager import UserManager
 from utils.utils import create_upload_path
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
     FILE_UPLOAD_PATH = 'accounts'
     SOCIAL_CHOICES = (("KAKAO", "kakao"), ("NAVER", "naver"), ("APPLE", "apple"), ("NONE", "none"))
 
-    username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(_("email_address"), unique=True)
+    email = models.EmailField("email", unique=True)
     social = models.CharField(max_length=20, choices=SOCIAL_CHOICES, null=True, blank=True, default="NONE")
 
     nickname = models.CharField(max_length=30, null=False, blank=False, default='')
@@ -21,12 +20,19 @@ class User(AbstractUser):
     profile = models.ImageField(upload_to=create_upload_path, blank=True, null=True)
     bio = models.TextField(max_length=300, blank=True, null=True)
 
-    created_date = models.DateField(auto_now=True)
+    created_date = models.DateTimeField(auto_now=True)
+    last_login_date = models.DateTimeField(null=True, blank=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username", "birth_day", "nickname"]
+    REQUIRED_FIELDS = ["birth_day", "nickname"]
     
     object = UserManager()
+
+    def get_username(self):
+        return self.email
 
     def __str__(self):
         return self.nickname
